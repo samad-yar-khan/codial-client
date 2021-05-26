f3b97a4f4362d32d882cedd3dc71f2e1be11232d
samad-yar-khan <70485812+samad-yar-khan@users.noreply.github.com>
2021-05-27 01:30:27 +0530

    Add navbar to the app

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import PropTypes from 'prop-types'
+import PropTypes from 'prop-types';
 
 import { fetchPosts } from '../actions/posts';
 
-
-import {PostsList} from './index';
+import { PostsList } from './index';
 
 class App extends React.Component {
   componentDidMount() {
@@ -18,7 +17,69 @@ class App extends React.Component {
 
     return (
       <div className="App">
-         <PostsList posts ={posts}/>
+        <nav className="nav">
+          <div className="left-div">
+            <img
+              className="site-logo"
+              src="https://image.flaticon.com/icons/png/512/3698/3698156.png"
+              alt="logo"
+            />
+          </div>
+          <div className="search-container">
+            <img
+              className="search-icon"
+              src="https://image.flaticon.com/icons/png/512/622/622669.png"
+              alt="search"
+            />
+
+            <input placeholder="Search" />
+
+            <div className="search-results">
+              <ul>
+                <li className="search-results-row">
+                  <img
+                    src="https://image.flaticon.com/icons/png/512/1946/1946429.png"
+                    alt="user-avatar"
+                  />
+                  <span>Sameer Khan</span>
+                </li>
+                <li className="search-results-row">
+                  <img
+                    src="https://image.flaticon.com/icons/png/512/1946/1946429.png"
+                    alt="user-avatar"
+                  />
+                  <span>Sameer Khan</span>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="right-nav">
+            <div className="user">
+              <img
+                src="https://image.flaticon.com/icons/png/512/1946/1946429.png"
+                alt="user-dp"
+                id="user-dp"
+              />
+              <span>Samad</span>        
+            </div>
+            <div className='nav-links'>
+               <ul>
+                  <li>
+                     Login
+                  </li>
+                  <li>
+                     Logout
+                  </li>
+                  <li>
+                     Register
+                  </li>
+
+               </ul>
+
+            </div>
+          </div>
+        </nav>
+        <PostsList posts={posts} />
       </div>
     );
   }
@@ -31,8 +92,8 @@ function mapStateToProps(state) {
 }
 
 App.propTypes = {
-   posts : PropTypes.array.isRequired
-}
+  posts: PropTypes.array.isRequired,
+};
 
 let connectedAppComponent = connect(mapStateToProps)(App);
 export default connectedAppComponent;
