38e1c3debec19ed64ee5b3442289cff523c2caa4
samad-yar-khan <70485812+samad-yar-khan@users.noreply.github.com>
2021-05-27 00:22:59 +0530

    Make post-list and fetch posts

--- src/components/App.js
+++ src/components/App.js
@@ -1,33 +1,92 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import {fetchPosts} from '../actions/posts'
+import { fetchPosts } from '../actions/posts';
 
 class App extends React.Component {
-   
-   componentDidMount() {
-      
-      this.props.dispatch(fetchPosts());
-      
-   }
-   
-   
-   render() {
+  componentDidMount() {
+    this.props.dispatch(fetchPosts());
+  }
 
-      console.log("PROPS ",this.props);
+  render() {
+    console.log('PROPS ', this.props);
+    const { posts } = this.props;
 
-      return (
-         <div>
-               HEY
-         </div>
-      );
-   }
+    return (
+      <div className="App">
+        <div className="post-list">
+          {posts.map((post) => {
+            return (
+              <div className="post-wrapper">
+                <div className="post-header">
+                  <div className="post-avatar">
+                    <img
+                      alt={'user'}
+                      src={
+                        'https://image.flaticon.com/icons/png/512/1946/1946429.png'
+                      }
+                    />
+
+                    <div className={'post-author'}>{post.user.name}</div>
+                    <div className="post-time">5 minutes ago</div>
+                  </div>
+
+                  <div className="post-content">{post.content}</div>
+
+                  <div className="post-actions">
+                    <div className="post-like">
+                      <img
+                        alt={'like'}
+                        src={
+                          'https://image.flaticon.com/icons/png/512/1077/1077035.png'
+                        }
+                      />
+                      <span>{post.likes.length}</span>
+                    </div>
+
+                    <div className="post-comment-icon">
+                      <img
+                        alt={'comments'}
+                        src={
+                          'https://image.flaticon.com/icons/png/512/1380/1380338.png'
+                        }
+                      />
+                      <span>{post.comments.length}</span>
+                    </div>
+                  </div>
+
+                  <div className="post-comment-box">
+                    <input placeholder="What are your thoughts ?" />
+                  </div>
+
+                  <div className="post-comments-list">
+                    <div className="post-comments-item">
+
+                      <div className="post-comment-header">
+                        <span className="post-comment-author">Samad</span>
+                        <span className="post-comment-time">a minute ago</span>
+                        <span className="post-comment-likes">22</span>
+                      </div>
+                        
+                     <div className='post-comment-content'>
+                        Random Comment 
+                     </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    );
+  }
 }
 
-function mapStateToProps(state){
-   return{
-      posts : state.posts
-   }
+function mapStateToProps(state) {
+  return {
+    posts: state.posts,
+  };
 }
 
-let connectedAppComponent = connect(mapStateToProps)(App)
+let connectedAppComponent = connect(mapStateToProps)(App);
 export default connectedAppComponent;
