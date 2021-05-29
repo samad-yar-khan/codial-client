3f3c581bdab31d84cfce6d64988dd1948c26b2e5
samad-yar-khan <70485812+samad-yar-khan@users.noreply.github.com>
2021-05-29 13:25:11 +0530

    Handle Register and Login Form using controlled react components

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,31 +1,66 @@
 import React from 'react';
 
 class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    // this.emailInputRef = React.createRef();
+    // this.passwordInputRef = React.createRef();
+    this.state = {
+      email: '',
+      password: '',
+    };
+  }
 
-    constructor(props){
-        super(props);
-        this.emailInputRef = React.createRef();
-        this.passwordInputRef = React.createRef();
+  handleEmailChange=(e)=>{
+      this.setState({
+          email:e.target.value
+      })
+  }
 
-    }
+  handlePasswordChange=(e)=>{
+    this.setState({
+        password:e.target.value
+    })
+}
 
-    handleFormSubmit=(e)=>{
-        e.preventDefault();
+  handleFormSubmit = (e) => {
+    e.preventDefault();
+    const {email ,  password} = this.state;
+    console.log('email' , email );
+    console.log('pass' , password)
 
-        console.log("email" , this.emailInputRef);
-        console.log("pass" , this.passwordInputRef);
-
-    }
+    // console.log("email" , this.emailInputRef);
+    // console.log("pass" , this.passwordInputRef);
+    this.setState({
+        email:'',
+        password:'',
+    })
+  };
 
   render() {
+
     return (
       <form method="post" action="#" className="login-form">
         <div className="login-signup-header">Login</div>
         <div className="field">
-          <input type="email" placeholder="Email" required ref={this.emailInputRef}/>
+          <input
+            type="email"
+            placeholder="Email"
+            required
+            // ref={this.emailInputRef}
+            onChange={this.handleEmailChange}
+            value={this.state.email}
+          />
         </div>
         <div className="field">
-          <input type="password" placeholder="Password" required ref={this.passwordInputRef}/>
+          <input
+            type="password"
+            placeholder="Password"
+            required
+            // ref={this.passwordInputRef}
+            onChange={this.handlePasswordChange}
+            value={this.state.password}
+          />
         </div>
         <div className="field">
           <button onClick={this.handleFormSubmit}>Login</button>
--- src/components/Register.js
+++ src/components/Register.js
@@ -1,21 +1,84 @@
 import React from 'react';
 
 class Register extends React.Component {
+  constructor(props) {
+    super(props);
+    // this.emailInputRef = React.createRef();
+    // this.passwordInputRef = React.createRef();
+    this.state = {
+      email: '',
+      password: '',
+      confirmedPassword: '',
+    };
+  }
+
+  handleEmailChange = (e) => {
+    this.setState({
+      email: e.target.value,
+    });
+  };
+
+  handlePasswordChange = (e) => {
+    this.setState({
+      password: e.target.value,
+    });
+  };
+
+  handleConfirmPasswordChange = (e) => {
+    this.setState({
+      confirmedPassword: e.target.value,
+    });
+  };
+
+  handleFormSubmit = (e) => {
+    e.preventDefault();
+    const { email, password , confirmedPassword} = this.state;
+    console.log('email', email);
+    console.log('pass', password);
+    console.log('c-pass', confirmedPassword);
+
+    // console.log("email" , this.emailInputRef);
+    // console.log("pass" , this.passwordInputRef);
+    this.setState({
+      email:'',
+      password:'',
+      confirmedPassword:''
+    })
+  };
+
   render() {
     return (
       <form method="post" action="#" className="login-form">
         <div className="login-signup-header">Register</div>
         <div className="field">
-          <input type="email" placeholder="Email" required />
+          <input 
+            type="email" 
+            placeholder="Email" 
+            onChange={this.handleEmailChange}
+            required 
+            value={this.state.email}
+          />
         </div>
         <div className="field">
-          <input type="password" placeholder="Password" required />
+          <input 
+            type="password" 
+            placeholder="Password" 
+            onChange={this.handlePasswordChange}
+            required 
+            value={this.state.password}
+          />
         </div>
         <div className="field">
-          <input type="password" placeholder="Confirm Password" required />
+          <input 
+            type="password" 
+            placeholder="Confirm Password" 
+            onChange={this.handleConfirmPasswordChange}
+            required 
+            value={this.state.confirmedPassword}
+          />
         </div>
         <div className="field">
-          <button>Register</button>
+          <button onClick={this.handleFormSubmit}>Register</button>
         </div>
       </form>
     );
