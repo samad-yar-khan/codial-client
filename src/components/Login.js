da779462a477d3b8fa2127387852627fd7c7a4db
samad-yar-khan <70485812+samad-yar-khan@users.noreply.github.com>
2021-05-29 12:46:31 +0530

    Bug fix - form

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,27 +1,22 @@
 import React from 'react';
 
 class Login extends React.Component {
-    render() {
-        return (
-            <form method='post' action='#'>
-                <div className='login-form'>
-                
-                    <div className='login-signup-header'>Login</div>
-                    <div className='field'>
-                        <input type='email' placeholder='Email' required/>
-                    </div>
-                    <div className='field'>
-                        <input type='password' placeholder='Password' required/>
-                    </div>
-                    <div className='field'>
-                        <button>
-                            Login
-                        </button>
-                    </div>
-                </div>
-            </form>
-        );
-    }
+  render() {
+    return (
+      <form method="post" action="#" className="login-form">
+        <div className="login-signup-header">Login</div>
+        <div className="field">
+          <input type="email" placeholder="Email" required />
+        </div>
+        <div className="field">
+          <input type="password" placeholder="Password" required />
+        </div>
+        <div className="field">
+          <button>Login</button>
+        </div>
+      </form>
+    );
+  }
 }
 
 export default Login;
--- src/components/Register.js
+++ src/components/Register.js
@@ -1,30 +1,25 @@
 import React from 'react';
 
 class Register extends React.Component {
-    render() {
-        return (
-            <form method='post' action='#'>
-                <div className='login-form'>
-                
-                    <div className='login-signup-header'>Register</div>
-                    <div className='field'>
-                        <input type='email' placeholder='Email' required/>
-                    </div>
-                    <div className='field'>
-                        <input type='password' placeholder='Password' required/>
-                    </div>
-                    <div className='field'>
-                        <input type='password' placeholder='Confirm Password' required/>
-                    </div>
-                    <div className='field'>
-                        <button>
-                            Register
-                        </button>
-                    </div>
-                </div>
-            </form>
-        );
-    }
+  render() {
+    return (
+      <form method="post" action="#" className="login-form">
+        <div className="login-signup-header">Register</div>
+        <div className="field">
+          <input type="email" placeholder="Email" required />
+        </div>
+        <div className="field">
+          <input type="password" placeholder="Password" required />
+        </div>
+        <div className="field">
+          <input type="password" placeholder="Confirm Password" required />
+        </div>
+        <div className="field">
+          <button>Register</button>
+        </div>
+      </form>
+    );
+  }
 }
 
 export default Register;
