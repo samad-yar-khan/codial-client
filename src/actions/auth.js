d1b4e07f1438ec148a007c2a02c51df21f4c38dc
samad-yar-khan <70485812+samad-yar-khan@users.noreply.github.com>
2021-06-01 17:07:13 +0530

    Create Reducer for Signin

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,4 +1,4 @@
-import { LOGIN_START, LOGIN_SUCCESS, LOGIN_FAIL } from './actionTypes';
+import { LOGIN_START, LOGIN_SUCCESS, LOGIN_FAIL ,SIGNUP_FAIL ,SIGNUP_SUCCESS ,SIGNUP_START} from './actionTypes';
 
 import { APIUrls } from '../helper/urls';
 
@@ -51,3 +51,52 @@ export function login(email, password) {
       });
   };
 }
+
+
+export function signin(email, password , confirmPassword ,  name) {
+    return function (dispatch) {
+      dispatch(startSignin()); //we are doing this to set the inprogress state prop of auth while we check tteh auth
+      const url = APIUrls.signup();
+      fetch(url, {
+        method: 'post',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: getFormbody(email , name , password, confirmPassword ), //this will send us a url encoded string with email and  pasword
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data);
+          if (data.success) {
+            //save user
+            dispatch(signinSuccess(data.data.user))
+            return;
+          } else {
+            dispatch(signinFailed(data.message));
+          }
+        });
+    };
+  }
+  
+
+
+export function startSignin() {
+    return {
+      type: SIGNUP_START,
+    };
+  }
+  
+  export function signinFailed(errorMessage) {
+    return {
+      type: SIGNUP_FAIL,
+      error: errorMessage,
+    };
+  }
+  
+  export function signinSuccess(user) {
+    return {
+      type: SIGNUP_SUCCESS,
+      user: user,
+    };
+  }
+  
--- src/helper/urls.js
+++ src/helper/urls.js
@@ -10,7 +10,7 @@ const API_ROOT = 'http://codeial.codingninjas.com:8000/api/v2';
 export const APIUrls = {
 
     login : ()=> `${API_ROOT}/users/login`,
-    sign : ()=> `${API_ROOT}/users/signup`,
+    signup : ()=> `${API_ROOT}/users/signup`,
     fetchPosts : (page =1 , limit = 5) => `${API_ROOT}/posts?page${page}&limit=${limit}`
 
 };
