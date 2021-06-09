96826d3d0fb67c44316cbc78a1e72a1a80e70545
samad-yar-khan <70485812+samad-yar-khan@users.noreply.github.com>
2021-06-09 15:13:22 +0530

    Re-Complete Edit User Action

--- src/actions/user.js
+++ src/actions/user.js
@@ -7,6 +7,42 @@ import {
 import { APIUrls } from '../helper/urls';
 import {getFormbody , getAuthTokenFromLocalStorage, setAuthTokenInLocalStorage} from '../helper/utils'
 
+export function editUser(userId , name , password , confirmPassword){
+    return (dispatch)=>{
+
+        const url = APIUrls.editUser;
+        fetch(url , {
+            method : "POST",
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+                'Authorization' : `Bearer ${getAuthTokenFromLocalStorage()}`
+            },
+            body : getFormbody({
+                name: name,
+                password: password,
+                confirm_password: confirm_password,
+                id:userId,
+            })
+
+        }).
+        then((response)=> response.json()).
+        then((data) => {
+            console.log('data',data);
+            if(data.success){
+                editUserSuccessful(data.data.user);
+                if(data.data.token){
+                    setAuthTokenInLocalStorage(data.data.token);
+                }
+            }else{
+                dispatc(editUserFailure(data.message));
+            }
+
+            return;
+        }).
+        catch((err)=> {console.log("ERROR :" ,err)});
+
+    }
+}
 
 export function editUserSuccessful(user){
     return {
