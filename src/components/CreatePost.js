da28e69be0b38613989ca85df46e143f8b398d5d
samad-yar-khan <70485812+samad-yar-khan@users.noreply.github.com>
2021-06-15 15:00:02 +0530

    Fix Createposts UI

--- src/components/CreatePost.js
+++ src/components/CreatePost.js
@@ -1,39 +1,40 @@
 import React from 'react';
 
 class CreatePost extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      content: '',
+    };
+  }
 
-    constructor(props){
-        super(props);
-        this.state = {
-            content : ""
-        }
-    }
+  handleChange = (e) => {
+    this.setState({
+      content: e.target.value,
+    });
+  };
 
-    handleChange =(e)=>{
-        this.setState({
-            content : e.target.value
-        })
-    }
+  handleOnClick = () => {
+    //action to d
+  };
 
-    handleOnClick=()=>{
-        //action to d
-    }
+  render() {
+    return (
+      <div className="create-post">
+        <textarea
+          className="add-post"
+          onChange={this.handleChange}
+          value={this.state.content}
+        />
 
-    render() {
-        return (
-            <div className="create-post">
-                
-                <textarea 
-                    className="add-post" 
-                    onChange={this.handleChange}
-                    value ={this.state.content}
-                />
-
-                <button id="add-post-btn" onClick={this.handleOnClick}>Post</button>
-    
-            </div>
-        );
-    }
+        <div>
+          <button id="add-post-btn" onClick={this.handleOnClick}>
+            Post
+          </button>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default CreatePost;
